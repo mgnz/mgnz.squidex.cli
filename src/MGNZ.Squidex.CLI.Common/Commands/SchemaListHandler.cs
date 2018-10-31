c9679512639b9605c716fbf2cf7422b7103e6880
Steven Higgan <stevenh@mad.geek.nz>
2018-11-01 00:19:39 +1300

    wip list and tag

--- src/MGNZ.Squidex.CLI.Common/Commands/SchemaListHandler.cs
+++ src/MGNZ.Squidex.CLI.Common/Commands/SchemaListHandler.cs
@@ -7,17 +7,24 @@ namespace MGNZ.Squidex.CLI.Common.Commands
 
   using MediatR;
 
+  using MGNZ.Squidex.Client;
   using MGNZ.Squidex.CLI.Common.Routing;
 
   using Serilog;
 
   public class SchemaListHandler : BaseHandler<SchemaListRequest>
   {
-    public SchemaListHandler(ILogger logger, IClientProxyFactory clientFactory, IContainer container) : base(logger, clientFactory, container) { }
+    public SchemaListHandler(ILogger logger, IClientProxyFactory clientFactory, IContainer container)
+      : base(logger, clientFactory, container)
+    {
+
+    }
 
     /// <inheritdoc />
     public override async Task<Unit> Handle(SchemaListRequest request, CancellationToken cancellationToken)
     {
+      var proxy = ClientFactory.GetClientProxy<ISquidexAppSchemaClient>(request.AliasCredentials);
+
       return await base.Handle(request, cancellationToken);
     }
   }
@@ -25,6 +32,7 @@ namespace MGNZ.Squidex.CLI.Common.Commands
   [Noun("schema"), Verb("list")]
   public class SchemaListRequest: BaseRequest
   {
+    [Option("app", "application", required: true, ordanalityOrder: 1)] public string Application { get; set; }
     [Option("c", "alias-credentials")] public string AliasCredentials { get; set; }
   }
 }
