91352364131c9bf76f62770aeaeeaefa0ee089ec
Steven Higgan <stevenh@mad.geek.nz>
2018-11-02 00:38:06 +1300

    making a start on the schema end to end test
    #24

---
+++ test/MGNZ.Squidex.CLI.Tests/Commands/SchemaHandlersIntegrationTest.cs
@@ -0,0 +1,45 @@
+namespace MGNZ.Squidex.CLI.Tests.Commands
+{
+  using System.IO;
+  using System.Reflection;
+  using System.Threading.Tasks;
+
+  using MGNZ.Squidex.CLI.Common.Commands;
+  using MGNZ.Squidex.CLI.Common.Configuration;
+  using MGNZ.Squidex.CLI.Common.Platform;
+  using MGNZ.Squidex.CLI.Tests.Platform;
+  using MGNZ.Squidex.CLI.Tests.Plumbing;
+
+  using Microsoft.Extensions.Configuration;
+
+  using Xunit;
+
+  [Collection("Sequential Squidex Integration Tests")]
+  [Trait("category", "squidex-cli-integration")]
+  public class SchemaHandlersIntegrationTest
+  {
+    readonly ApplicationConfiguration _applicationConfiguration = null;
+
+    public SchemaHandlersIntegrationTest()
+    {
+      var configurationRoot = TestHelper.GetConfigurationRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+      configurationRoot.Bind(_applicationConfiguration);
+    }
+
+    [Fact(Skip = "under development")]
+    //[Fact()]
+    public async Task Execute_EndToEnd()
+    {
+      var authTokenFactory = new OAuthTokenFactory(SerilogFixture.UsefullLogger<OAuthTokenFactory>(), _applicationConfiguration);
+      var clientFactory = new ClientProxyFactoryFixture() {ApplicationConfiguration = _applicationConfiguration, OAuthTokenFactory = authTokenFactory}.Build();
+      //var fileHandler = new FileHandlerMock() {ReadFile = FileHandlerMock.WithReadFileAsNoOp(), WriteFile = FileHandlerMock.WithWriteFileAsNoOp()}.Build();
+      var fileHandler = new FileHandler(SerilogFixture.UsefullLogger<FileHandler>());
+
+      var import = new SchemaImportHandler(SerilogFixture.UsefullLogger<SchemaImportHandler>(), clientFactory, fileHandler, null);
+      //var contentExport = new ContentExportHandler(SerilogFixture.UsefullLogger<ContentExportHandler>(), clientFactory, fileHandler, null);
+      //var contentDelete = new ContentDeleteHandler(SerilogFixture.UsefullLogger<ContentDeleteHandler>(), clientFactory, null);
+
+
+    }
+  }
+}
