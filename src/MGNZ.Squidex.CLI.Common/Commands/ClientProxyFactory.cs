f1156e74e251ef415d3fa743c9b094bfef44e285
Steven Higgan <stevenh@mad.geek.nz>
2018-11-02 00:09:51 +1300

    adding note for #27

--- src/MGNZ.Squidex.CLI.Common/Commands/ClientProxyFactory.cs
+++ src/MGNZ.Squidex.CLI.Common/Commands/ClientProxyFactory.cs
@@ -75,6 +75,8 @@ namespace MGNZ.Squidex.CLI.Common.Commands
     private TProxy CreateProxy<TProxy>(string aliasCredentials, Uri baseAddress)
       where TProxy : class
     {
+      // todo : https://github.com/mgnz/mgnz.squidex.cli/issues/27
+
       return RestService.For<TProxy>(
         new HttpClient(
           new SimpleAccessTokenHttpClientHandler(
