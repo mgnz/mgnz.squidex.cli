6e0c9a935ef56e64d7e3b4bd0176c203c23ce025
Steven Higgan <stevenh@mad.geek.nz>
2018-11-08 23:39:14 +1300

    common testing infrastructure

--- src/MGNZ.Squidex.CLI.Common/Commands/SquidexAppSchemaClientExtensions.cs
+++ src/MGNZ.Squidex.CLI.Common/Commands/SquidexAppSchemaClientExtensions.cs
@@ -8,14 +8,20 @@ namespace MGNZ.Squidex.CLI.Common.Commands
 {
   public static class SquidexAppSchemaClientExtensions
   {
-    public static async Task<bool> SchemaExists(this ISquidexAppSchemaClient that, string application, string name)
+    public static async Task<bool> SchemaExists(this ISquidexAppSchemaClient that, string application, string name = null)
     {
       var data = await that.GetAllSchemas(application);
 
       var count = Convert.ToInt32(data.Count);
-      var any = ((IEnumerable<dynamic>) data).Any(d => CheckEquality(d, name));
-
-      return count != 0 && any;
+      if (string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name))
+      {
+        return count > 0;
+      }
+      else
+      {
+        var any = ((IEnumerable<dynamic>)data).Any(d => CheckEquality(d, name));
+        return count != 0 && any;
+      }
     }
 
     private static bool CheckEquality(dynamic d, string name)
