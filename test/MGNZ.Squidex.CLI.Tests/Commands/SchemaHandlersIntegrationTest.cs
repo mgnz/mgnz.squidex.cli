22b48641edfd67c6c7c46abff20a440c25ce9709
Steven Higgan <stevenh@mad.geek.nz>
2018-11-08 23:40:25 +1300

    starting to seperate the tests
    #24

--- test/MGNZ.Squidex.CLI.Tests/Commands/SchemaHandlersIntegrationTest.cs
+++ test/MGNZ.Squidex.CLI.Tests/Commands/SchemaHandlersIntegrationTest.cs
@@ -1,12 +1,14 @@
-using System.Threading;
-
 namespace MGNZ.Squidex.CLI.Tests.Commands
 {
+  using System;
   using System.IO;
   using System.Reflection;
+  using System.Threading;
   using System.Threading.Tasks;
 
   using FluentAssertions;
+
+  using MediatR;
 
   using MGNZ.Squidex.Client;
   using MGNZ.Squidex.CLI.Common.Commands;
@@ -23,66 +25,114 @@ namespace MGNZ.Squidex.CLI.Tests.Commands
   [Trait("category", "squidex-cli-integration")]
   public class SchemaHandlersIntegrationTest
   {
-    readonly ApplicationConfiguration _applicationConfiguration = null;
+    private readonly SchemaImportHandler _schemaImportHandler = null;
+    private readonly SchemaExportHandler _schemaExportHandler = null;
+    private readonly SchemaDeleteHandler _schemaDeleteHandler = null;
+    private readonly SchemaListHandler _schemaListHandler = null;
+
+    private readonly ISquidexAppSchemaClient _checker = null;
 
     public SchemaHandlersIntegrationTest()
     {
-      _applicationConfiguration = new ApplicationConfiguration();
+      var _applicationConfiguration = new ApplicationConfiguration();
       var configurationRoot = TestHelper.GetConfigurationRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
       configurationRoot.Bind(_applicationConfiguration);
-    }
 
-    [Fact()]
-    public async Task Execute_EndToEnd()
-    {
       var consoleWriter = new ConsoleWriter(SerilogFixture.UsefullLogger<ConsoleWriter>());
       var authTokenFactory = new OAuthTokenFactory(SerilogFixture.UsefullLogger<OAuthTokenFactory>(), _applicationConfiguration);
       var clientFactory = new ClientProxyFactory(SerilogFixture.UsefullLogger<ClientProxyFactory>(), authTokenFactory, _applicationConfiguration);
       //var fileHandler = new FileHandlerMock() {ReadFile = FileHandlerMock.WithReadFileAsNoOp(), WriteFile = FileHandlerMock.WithWriteFileAsNoOp()}.Build();
       var fileHandler = new FileHandler(SerilogFixture.UsefullLogger<FileHandler>());
 
-      var schemaImportHandler = new SchemaImportHandler(SerilogFixture.UsefullLogger<SchemaImportHandler>(), clientFactory, consoleWriter, null);
-      var schemaExportHandler = new SchemaExportHandler(SerilogFixture.UsefullLogger<SchemaExportHandler>(), clientFactory, consoleWriter, null);
-      var schemaDeleteHandler = new SchemaDeleteHandler(SerilogFixture.UsefullLogger<SchemaDeleteHandler>(), clientFactory, consoleWriter, null);
-      //var schemaListHandler = new SchemaDeleteHandler(SerilogFixture.UsefullLogger<ContentDeleteHandler>(), clientFactory, null);
+      _schemaImportHandler = new SchemaImportHandler(SerilogFixture.UsefullLogger<SchemaImportHandler>(), clientFactory, consoleWriter, null);
+      _schemaExportHandler = new SchemaExportHandler(SerilogFixture.UsefullLogger<SchemaExportHandler>(), clientFactory, consoleWriter, null);
+      _schemaDeleteHandler = new SchemaDeleteHandler(SerilogFixture.UsefullLogger<SchemaDeleteHandler>(), clientFactory, consoleWriter, null);
+      _schemaListHandler = new SchemaListHandler(SerilogFixture.UsefullLogger<SchemaListHandler>(), clientFactory, consoleWriter, null);
       //var schemaTagHandler = new SchemaTagHandler(SerilogFixture.UsefullLogger<ContentDeleteHandler>(), clientFactory, null);
 
-      var checker = clientFactory.GetClientProxy<ISquidexAppSchemaClient>("aut-developer");
+      _checker = clientFactory.GetClientProxy<ISquidexAppSchemaClient>("aut-developer");
+    }
 
-      var schemaImportResult = await schemaImportHandler.Handle(new SchemaImportRequest()
+    [Fact]
+    public async Task SchemaImport_Execute_EndToEnd()
+    {
+      await _checker.AssertNoSchemasExist("aut", delay: TimeSpan.FromSeconds(0.5));
+
+      await ImportSchemaStory(_schemaImportHandler);
+      await _checker.AssertSchemaMustExist("aut", "test-schema-1", delay: TimeSpan.FromSeconds(0.5));
+
+      await DeleteSchemaStory(_schemaDeleteHandler);
+      await _checker.AssertNoSchemasExist("aut", delay: TimeSpan.FromSeconds(0.5));
+    }
+
+    [Fact]
+    public async Task SchemaExport_Execute_EndToEnd()
+    {
+      await _checker.AssertNoSchemasExist("aut");
+      await ImportSchemaStory(_schemaImportHandler);
+
+      await ExportSchemaStory(_schemaExportHandler);
+      var exportedFileExists = File.Exists(Path.Combine(AssetLoader.ExportPath, $"{nameof(SchemaHandlersIntegrationTest)} {nameof(SchemaExport_Execute_EndToEnd)}-out.json"));
+      exportedFileExists.Should().BeTrue($"{nameof(SchemaExportRequest)} failed to export file");
+
+      // todo validate export file
+
+      await DeleteSchemaStory(_schemaDeleteHandler);
+      await _checker.AssertNoSchemasExist("aut", delay: TimeSpan.FromSeconds(0.5));
+    }
+
+    [Fact]
+    public async Task SchemaDelete_Execute_EndToEnd()
+    {
+      await _checker.AssertNoSchemasExist("aut", delay: TimeSpan.FromSeconds(0.5));
+      await ImportSchemaStory(_schemaImportHandler);
+
+      await DeleteSchemaStory(_schemaDeleteHandler);
+      await _checker.AssertSchemaMustNotExist("aut", "test-schema-1", delay: TimeSpan.FromSeconds(5));
+
+      await _checker.AssertNoSchemasExist("aut", delay: TimeSpan.FromSeconds(0.5));
+    }
+
+    [Fact(Skip = "tag not implemented")]
+    public async Task SchemaListt_Execute_EndToEnd()
+    {
+    }
+
+    [Fact(Skip = "tag not implemented")]
+    public async Task SchemaTag_Execute_EndToEnd()
+    {
+    }
+
+    private async Task<Unit> DeleteSchemaStory(SchemaDeleteHandler schemaDeleteHandler)
+    {
+      return await schemaDeleteHandler.Handle(new SchemaDeleteRequest()
+      {
+        AliasCredentials = "aut-developer",
+        Application = "aut",
+        Name = "test-schema-1"
+      }, CancellationToken.None);
+    }
+
+    private async Task<Unit> ExportSchemaStory(SchemaExportHandler schemaExportHandler)
+    {
+      return await schemaExportHandler.Handle(new SchemaExportRequest()
+      {
+        AliasCredentials = "aut-developer",
+        Application = "aut",
+        Name = "test-schema-1",
+        Path = Path.Combine(AssetLoader.ExportPath, $"{nameof(SchemaHandlersIntegrationTest)}-out.json")
+      }, CancellationToken.None);
+    }
+
+    private async Task<Unit> ImportSchemaStory(SchemaImportHandler schemaImportHandler)
+    {
+      return await schemaImportHandler.Handle(new SchemaImportRequest()
       {
         AliasCredentials = "aut-developer",
         Application = "aut",
         Name = "test-schema-1",
         Path = AssetLoader.Schema1Path
       }, CancellationToken.None);
-
-      var importedSchemaExists = await checker.SchemaExists("aut", "test-schema-1");
-      importedSchemaExists.Should().BeTrue($"{nameof(SchemaImportRequest)} failed to import file");
-
-      var schemaExportResult = await schemaExportHandler.Handle(new SchemaExportRequest()
-      {
-        AliasCredentials = "aut-developer",
-        Application = "aut",
-        Name = "test-schema-1",
-        Path = Path.Combine(AssetLoader.ExportPath, $"{nameof(SchemaHandlersIntegrationTest)} {nameof(Execute_EndToEnd)} out.json")
-      }, CancellationToken.None);
-
-      var exportedFileExists = File.Exists(Path.Combine(AssetLoader.ExportPath, $"{nameof(SchemaHandlersIntegrationTest)} {nameof(Execute_EndToEnd)} out.json"));
-      exportedFileExists.Should().BeTrue($"{nameof(SchemaExportRequest)} failed to export file");
-
-      var existingSchemaExists = await checker.SchemaExists("aut", "test-schema-1");
-      existingSchemaExists.Should().BeTrue($"{nameof(SchemaDeleteRequest)} dependent schema does not exist");
-
-      var schemaDeleteResult = await schemaDeleteHandler.Handle(new SchemaDeleteRequest()
-      {
-        AliasCredentials = "aut-developer",
-        Application = "aut",
-        Name = "test-schema-1"
-      }, CancellationToken.None);
-
-      var schemaIsDeleted = await checker.SchemaExists("aut", "test-schema-1");
-      schemaIsDeleted.Should().BeFalse($"{nameof(SchemaDeleteRequest)} dependent schema must not exist");
     }
   }
 }
