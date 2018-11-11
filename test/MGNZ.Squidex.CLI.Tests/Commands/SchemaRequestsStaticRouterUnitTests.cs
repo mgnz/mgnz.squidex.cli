4f42a405956ab378181a164dbf834356ba393a25
Steven Higgan <stevenh@mad.geek.nz>
2018-11-11 19:24:30 +1300

    static router tests for all Schema commands
    #24

--- test/MGNZ.Squidex.CLI.Tests/Commands/SchemaRequestsStaticRouterUnitTests.cs
+++ test/MGNZ.Squidex.CLI.Tests/Commands/SchemaRequestsStaticRouterUnitTests.cs
@@ -13,6 +13,10 @@ namespace MGNZ.Squidex.CLI.Tests.Commands
   public class SchemaRequestsStaticRouterUnitTests : BaseRequestStaticRouterUnitTest
   {
     public static List<object[]> StaticRouter_Verify_SchemaImport_Data => BuildSchemaImportData("schema", "import").ToList();
+    public static List<object[]> StaticRouter_Verify_SchemaExport_Data => BuildSchemaImportData("schema", "export").ToList();
+    public static List<object[]> StaticRouter_Verify_SchemaDelete_Data => BuildSchemaImportData("schema", "delete").ToList();
+    public static List<object[]> StaticRouter_Verify_SchemaList_Data => BuildSchemaImportData("schema", "list").ToList();
+    public static List<object[]> StaticRouter_Verify_SchemaTag_Data => BuildSchemaImportData("schema", "tag").ToList();
 
     [Theory]
     [MemberData(nameof(StaticRouter_Verify_SchemaImport_Data))]
@@ -27,6 +31,70 @@ namespace MGNZ.Squidex.CLI.Tests.Commands
 
       actual.Should().NotBeNull();
       actual.Should().BeAssignableTo<SchemaImportRequest>();
+      actual.Should().BeEquivalentTo(expectedRequest);
+    }
+
+    [Theory]
+    [MemberData(nameof(StaticRouter_Verify_SchemaExport_Data))]
+    public void StaticRouter_Verify_SchemaExport(SchemaExportRequest expectedRequest, string inCommandLine)
+    {
+      var router = new StaticCommandLineRouterFixture()
+      {
+        WithRouteValidator = new RouteRequestValidatorFixture().Mock()
+      }.Build();
+
+      var actual = router.GetOne(RoutingMetadata.Value.Values, inCommandLine.Split(' '));
+
+      actual.Should().NotBeNull();
+      actual.Should().BeAssignableTo<SchemaExportRequest>();
+      actual.Should().BeEquivalentTo(expectedRequest);
+    }
+
+    [Theory]
+    [MemberData(nameof(StaticRouter_Verify_SchemaDelete_Data))]
+    public void StaticRouter_Verify_SchemaDelete(SchemaDeleteRequest expectedRequest, string inCommandLine)
+    {
+      var router = new StaticCommandLineRouterFixture()
+      {
+        WithRouteValidator = new RouteRequestValidatorFixture().Mock()
+      }.Build();
+
+      var actual = router.GetOne(RoutingMetadata.Value.Values, inCommandLine.Split(' '));
+
+      actual.Should().NotBeNull();
+      actual.Should().BeAssignableTo<SchemaDeleteRequest>();
+      actual.Should().BeEquivalentTo(expectedRequest);
+    }
+
+    [Theory]
+    [MemberData(nameof(StaticRouter_Verify_SchemaList_Data))]
+    public void StaticRouter_Verify_SchemaList(SchemaListRequest expectedRequest, string inCommandLine)
+    {
+      var router = new StaticCommandLineRouterFixture()
+      {
+        WithRouteValidator = new RouteRequestValidatorFixture().Mock()
+      }.Build();
+
+      var actual = router.GetOne(RoutingMetadata.Value.Values, inCommandLine.Split(' '));
+
+      actual.Should().NotBeNull();
+      actual.Should().BeAssignableTo<SchemaListRequest>();
+      actual.Should().BeEquivalentTo(expectedRequest);
+    }
+
+    [Theory]
+    [MemberData(nameof(StaticRouter_Verify_SchemaTag_Data))]
+    public void StaticRouter_Verify_SchemaTag(SchemaTagRequest expectedRequest, string inCommandLine)
+    {
+      var router = new StaticCommandLineRouterFixture()
+      {
+        WithRouteValidator = new RouteRequestValidatorFixture().Mock()
+      }.Build();
+
+      var actual = router.GetOne(RoutingMetadata.Value.Values, inCommandLine.Split(' '));
+
+      actual.Should().NotBeNull();
+      actual.Should().BeAssignableTo<SchemaTagRequest>();
       actual.Should().BeEquivalentTo(expectedRequest);
     }
   }
