0d32b1cd586be225c9e82af55aefa6a2cb8148ce
Steven Higgan <stevenh@mad.geek.nz>
2018-11-14 23:25:45 +1300

    updating asset loader stuff with posts; imports; query response

--- test/MGNZ.Squidex.CLI.Tests/Assets/AssetLoader.cs
+++ test/MGNZ.Squidex.CLI.Tests/Assets/AssetLoader.cs
@@ -1,4 +1,4 @@
-namespace MGNZ.Squidex.CLI.Tests
+namespace MGNZ.Squidex.CLI.Tests.Assets
 {
   using System;
   using System.IO;
@@ -12,31 +12,36 @@ namespace MGNZ.Squidex.CLI.Tests
   [Trait("category", "squidex-api-integration")]
   public class AssetLoader
   {
-    private string ns => typeof(AssetLoader).Namespace;
+    private static string ns => typeof(AssetLoader).Namespace;
 
-    public Lazy<dynamic> Schema1 => LoadAsset($"{ns}.schema1.json");
-    public Lazy<dynamic> Schema1DataQuery => LoadAsset($"{ns}.schema1.data.query.json");
-    public Lazy<dynamic> Schema1Data1Query => LoadAsset($"{ns}.schema1.data.1.query.json");
-    public Lazy<dynamic> Schema1Data1Post => LoadAsset($"{ns}.schema1.data.1.post.json");
-    public Lazy<dynamic> Schema1Data1PostResponse => LoadAsset($"{ns}.schema1.data.1.post.response.json");
+    public static Lazy<dynamic> Schema1 => LoadAsset($"{ns}.schema1.json");
+    public static Lazy<dynamic> Schema1Data1Post => new Lazy<dynamic>(LoadAsset($"{ns}.schema1.data.1.post.json"));
+    public static Lazy<dynamic> Schema1Data1PostResponse => new Lazy<dynamic>(LoadAsset($"{ns}.schema1.data.1.post.response.json"));
+    public static Lazy<dynamic> Schema1Data2Post => new Lazy<dynamic>(LoadAsset($"{ns}.schema1.data.2.post.json"));
+    public static Lazy<dynamic> Schema1Data2PostResponse => new Lazy<dynamic>(LoadAsset($"{ns}.schema1.data.2.post.response.json"));
+    public static Lazy<dynamic> Schema1DataImport => new Lazy<dynamic>(LoadAsset($"{ns}.schema1.data.import.json"));
+    public static Lazy<dynamic> Schema1DataImportResponse => new Lazy<dynamic>(LoadAsset($"{ns}.schema1.data.import.response.json"));
+    public static Lazy<dynamic> Schema1DataQueryResponse => new Lazy<dynamic>(LoadAsset($"{ns}.schema1.data.query.response.json"));
 
 
     public static string ExecutingPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
     public static string AssetPath => Path.Combine(ExecutingPath, "Assets");
     public static string Schema1Path => Path.Combine(AssetPath, "schema1.json");
-    public static string Schema1DataQueryPath => Path.Combine(AssetPath, "schema1.data.query.json");
-    public static string Schema1Data1QueryPath => Path.Combine(AssetPath, "schema1.data.1.query.json");
     public static string Schema1Data1PostPath => Path.Combine(AssetPath, "schema1.data.1.post.json");
     public static string Schema1Data1PostResponsePath => Path.Combine(AssetPath, "schema1.data.1.post.response.json");
+    public static string Schema1Data2PostPath => Path.Combine(AssetPath, "schema1.data.2.post.json");
+    public static string Schema1Data2PostResponsePath => Path.Combine(AssetPath, "schema1.data.2.post.response.json");
+    public static string Schema1DataImportPath => Path.Combine(AssetPath, "schema1.data.import.json");
+    public static string Schema1DataImportResponsePath => Path.Combine(AssetPath, "schema1.data.import.response.json");
 
     public static string ExportPath => Path.Combine(ExecutingPath, "Exports");
 
-    public dynamic LoadAsset(string path) => JsonConvert.DeserializeObject(StreamToString(GetManifestResourceStream(path)));
+    public static dynamic LoadAsset(string path) => JsonConvert.DeserializeObject<dynamic>(StreamToString(GetManifestResourceStream(path)));
 
-    private Stream GetManifestResourceStream(string fullyQualifiedNamespace) => GetManifestResourceStream(typeof(AssetLoader).GetTypeInfo().Assembly, fullyQualifiedNamespace);
-    private Stream GetManifestResourceStream(Assembly assembly, string fullyQualifiedNamespace) => assembly.GetManifestResourceStream(fullyQualifiedNamespace);
+    private static Stream GetManifestResourceStream(string fullyQualifiedNamespace) => GetManifestResourceStream(typeof(AssetLoader).GetTypeInfo().Assembly, fullyQualifiedNamespace);
+    private static Stream GetManifestResourceStream(Assembly assembly, string fullyQualifiedNamespace) => assembly.GetManifestResourceStream(fullyQualifiedNamespace);
 
-    protected string StreamToString(Stream inputStream)
+    protected static string StreamToString(Stream inputStream)
     {
       using (var reader = new StreamReader(inputStream))
         return reader.ReadToEnd();
